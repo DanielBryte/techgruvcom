70bf6f579ebc7645cf405d3f3588cdb1ccd530cc
danielbryte <danielbryte1@gmail.com>
2022-08-13 01:52:49 +0100

    search feature

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,4 @@
 const { DateTime }  = require("luxon");
-
 module.exports = function(eleventyConfig)  {
     eleventyConfig.addPassthroughCopy('./src/blog.css');
     eleventyConfig.addPassthroughCopy('./src/assets');
@@ -13,7 +12,30 @@ module.exports = function(eleventyConfig)  {
 
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
     })
+
+    require("dotenv").config();
+    const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+    const algoliasearch = require("algoliasearch");
     
+    const client = algoliasearch(process.env.ALGOLIA_APP, process.env.ALGOLIA_SEARCH_KEY);
+    const index = client.initIndex(process.env.ALGOLIA_INDEX);
+    
+    module.exports = function (eleventyConfig) {
+      eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+        name: "search", // The serverless function name from your permalink object
+        functionsDir: "./netlify/functions/",
+      });
+    
+      eleventyConfig.addFilter("getResults", function (query) {
+        return index.search(query, {
+          attributesToRetrieve: ["title", "url", "date", "description"],
+    
+        }).then(res => {
+          return res.hits;
+        })
+      });
+    
+    };
     return {
         dir: {
             input: "src",
